67734f7070a8cce6eb12d5cdd85630650fb491f3
lu92 <biklucjan@gmail.com>
2018-11-11 18:50:40 +0100

    [6] Create panel with list of coins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {CoinPanelComponentComponent} from './components/coin-panel-component/coin-panel-component.component';
+import {CoinListComponentComponent} from './components/coin-list-component/coin-list-component.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoinPanelComponentComponent
+    CoinPanelComponentComponent,
+    CoinListComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/coin-list-component/coin-list-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coin-list-component',
+  templateUrl: './coin-list-component.component.html',
+  styleUrls: ['./coin-list-component.component.scss']
+})
+export class CoinListComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
