39fd7d77f2bb7081c492c8243371b4b73a2d9c9c
lu92 <biklucjan@gmail.com>
2018-11-25 19:46:14 +0100

    [4] setup fusion charts lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {CoinPanelComponentComponent} from './components/coin-panel-component/coin-panel-component.component';
 import {LandingPageComponentComponent} from './components/landing-page-component/landing-page-component.component';
+import { ChartModule } from 'angular2-highcharts';
 
+declare var require: any;
 
 @NgModule({
   declarations: [
@@ -16,10 +19,13 @@ import {LandingPageComponentComponent} from './components/landing-page-component
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
-  ],
+    HttpClientModule,
+    ChartModule.forRoot(require('highcharts')),
+],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
 }
+platformBrowserDynamic().bootstrapModule(AppModule);
+
--- src/app/components/landing-page-component/landing-page-component.component.ts
+++ src/app/components/landing-page-component/landing-page-component.component.ts
@@ -3,11 +3,80 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-landing-page-component',
   templateUrl: './landing-page-component.component.html',
-  styleUrls: ['./landing-page-component.component.scss']
+  styleUrls: ['./landing-page-component.component.scss'],
 })
 export class LandingPageComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+    this.options = {
+      chart: {
+        type: 'column',
+        backgroundColor: '#44494D'
+      },
+      title: {
+        text: 'Summary of investment',
+        style: { 'color': '#88898A', 'fontSize': '24px' }
+      },
+      xAxis: {
+        categories: ['BTC', 'LTC', 'ETH', 'XRP', 'XRM', 'XLM', 'BNB']
+      },
+      yAxis: {
+        min: -200,
+        title: {
+          text: ''
+        },
+        gridLineColor: '#88898A',
+        lineColor: '#44494D',
+        stackLabels: {
+          enabled: false,
+          style: {
+            fontWeight: 'bold',
+            color: 'red'
+          }
+        }
+      },
+      legend: {
+        align: 'right',
+        x: -30,
+        verticalAlign: 'top',
+        y: 25,
+        floating: true,
+        backgroundColor: 'white',
+        borderColor: '#CCC',
+        borderWidth: 1,
+        shadow: false
+      },
+      tooltip: {
+        headerFormat: '<b>{point.x}</b><br/>',
+        pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+      },
+      plotOptions: {
+        column: {
+          stacking: 'normal',
+          dataLabels: {
+            enabled: true,
+            color:  'white'
+          }
+        }
+      },
+      series: [{
+        name: 'Profit',
+        color: '#44BBA4',
+        data: [400, 0, 40, 700, 20, 0, 0]
+      }, {
+        name: 'Investment',
+        color: '#88898A',
+        data: [200, 200, 30, 200, 800, 100, 700]
+      }, {
+        name: 'Lost',
+        color: '#F4796B',
+        data: [0, -100, 0, 0, 0, -80, 0]
+      }]
+    };
+  }
+
+  options: Object;
 
   ngOnInit() {
   }
