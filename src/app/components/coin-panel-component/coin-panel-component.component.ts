9c93eada4dbab1498d1a7a437d9888dc18f71c1d
lu92 <biklucjan@gmail.com>
2018-11-28 01:56:51 +0100

    [9] fill landing page with sample data

--- src/app/components/coin-panel-component/coin-panel-component.component.ts
+++ src/app/components/coin-panel-component/coin-panel-component.component.ts
@@ -1,15 +1,14 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Cryptocurrency} from '../../model/cryptocurrency';
 
 @Component({
   selector: 'app-coin-panel-component',
   templateUrl: './coin-panel-component.component.html',
   styleUrls: ['./coin-panel-component.component.css']
 })
-export class CoinPanelComponentComponent implements OnInit {
+export class CoinPanelComponentComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  @Input()
+  coin: Cryptocurrency;
 
 }
--- src/app/components/landing-page-component/landing-page-component.component.ts
+++ src/app/components/landing-page-component/landing-page-component.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {Cryptocurrency} from '../../model/cryptocurrency';
 
 @Component({
   selector: 'app-landing-page-component',
   templateUrl: './landing-page-component.component.html',
   styleUrls: ['./landing-page-component.component.scss'],
 })
-export class LandingPageComponentComponent implements OnInit {
+export class LandingPageComponentComponent {
+
+  btc = new Cryptocurrency('BTC', 0.5, 4000.0, 2200.0, 200.0, 10.0);
+  ltc = new Cryptocurrency('LTC', 3.4, 35.0, 100.0, 5.0, 5.0);
+  eth = new Cryptocurrency('ETH', 2.0, 100, 230.0, 30.0, 15.0);
+  xrp = new Cryptocurrency('XRP', 222.0, 0.4, 111.0, 30.0, 17.7);
+  xrm = new Cryptocurrency('XRM', 1.3, 90, 111.0, 10.0, 7.7);
+  xlm = new Cryptocurrency('XLM', 500, 0.34, 155.9, 35.4, 8.7);
+  iota = new Cryptocurrency('IOTA', 300, 0.24, 135.9, -15.4, -8.7);
+  bnb = new Cryptocurrency('BNB', 45, 4.2, 145.9, -5.4, -2.7);
+  vet = new Cryptocurrency('VET', 4000, 0.002, 133.9, -15.4, -12.7);
+  ada = new Cryptocurrency('ADA', 5000, 0.02, 1333.9, -15.4, -0.07);
 
   constructor() {
 
@@ -19,7 +31,7 @@ export class LandingPageComponentComponent implements OnInit {
         style: { 'color': '#88898A', 'fontSize': '24px' }
       },
       xAxis: {
-        categories: ['BTC', 'LTC', 'ETH', 'XRP', 'XRM', 'XLM', 'BNB']
+        categories: ['BTC', 'LTC', 'ETH', 'XRP', 'XRM', 'XLM', 'IOTA', 'BNB', 'VET', 'ADA']
       },
       yAxis: {
         min: -200,
@@ -63,22 +75,19 @@ export class LandingPageComponentComponent implements OnInit {
       series: [{
         name: 'Profit',
         color: '#44BBA4',
-        data: [400, 0, 40, 700, 20, 0, 0]
+        data: [400, 0, 40, 700, 20, 0, 0, 0, 50, 40]
       }, {
         name: 'Investment',
         color: '#88898A',
-        data: [200, 200, 30, 200, 800, 100, 700]
+        data: [200, 200, 30, 2000, 800, 100, 700, 300, 5000, 100]
       }, {
         name: 'Lost',
         color: '#F4796B',
-        data: [0, -100, 0, 0, 0, -80, 0]
+        data: [0, -100, 0, 0, 0, -80, 0, -90, 0, 0]
       }]
     };
   }
 
   options: Object;
-
-  ngOnInit() {
-  }
 
 }
